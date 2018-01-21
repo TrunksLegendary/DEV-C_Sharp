2e3ba6b867ce289caf4fd2e5ca2f84ac41ce3828
TrunksLegendary <trunks0408@gmail.com>
2018-01-21 12:16:05 -0500

    Restore
    oops

--- PlayGround/PlayGround/Form1.Designer.cs
+++ PlayGround/PlayGround/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PlayGround
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,141 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.TxtB_Output = new System.Windows.Forms.TextBox();
+            this.TxtB_Input = new System.Windows.Forms.TextBox();
+            this.Lbl_Input = new System.Windows.Forms.Label();
+            this.Lbl_Output = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(261, 93);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 23;
-            this.label3.Text = "label3";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 93);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 22;
-            this.label2.Text = "label2";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(264, 109);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(246, 363);
-            this.textBox2.TabIndex = 20;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 109);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(246, 363);
-            this.textBox1.TabIndex = 19;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(417, 483);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(75, 23);
-            this.button7.TabIndex = 18;
-            this.button7.Text = "button7";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(426, 19);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
-            this.button6.TabIndex = 17;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(345, 19);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 16;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(264, 19);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 15;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(183, 19);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 14;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(102, 19);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 13;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(21, 19);
+            this.button1.Location = new System.Drawing.Point(13, 13);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 12;
+            this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // contextMenuStrip1
+            // TxtB_Output
             // 
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.TxtB_Output.Location = new System.Drawing.Point(379, 156);
+            this.TxtB_Output.Multiline = true;
+            this.TxtB_Output.Name = "TxtB_Output";
+            this.TxtB_Output.Size = new System.Drawing.Size(356, 372);
+            this.TxtB_Output.TabIndex = 2;
             // 
-            // Form1
+            // TxtB_Input
+            // 
+            this.TxtB_Input.Location = new System.Drawing.Point(12, 156);
+            this.TxtB_Input.Multiline = true;
+            this.TxtB_Input.Name = "TxtB_Input";
+            this.TxtB_Input.Size = new System.Drawing.Size(356, 372);
+            this.TxtB_Input.TabIndex = 3;
+            // 
+            // Lbl_Input
+            // 
+            this.Lbl_Input.AutoSize = true;
+            this.Lbl_Input.Location = new System.Drawing.Point(9, 140);
+            this.Lbl_Input.Name = "Lbl_Input";
+            this.Lbl_Input.Size = new System.Drawing.Size(55, 13);
+            this.Lbl_Input.TabIndex = 4;
+            this.Lbl_Input.Text = "Input Text";
+            // 
+            // Lbl_Output
+            // 
+            this.Lbl_Output.AutoSize = true;
+            this.Lbl_Output.Location = new System.Drawing.Point(376, 140);
+            this.Lbl_Output.Name = "Lbl_Output";
+            this.Lbl_Output.Size = new System.Drawing.Size(63, 13);
+            this.Lbl_Output.TabIndex = 5;
+            this.Lbl_Output.Text = "Output Text";
+            // 
+            // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 519);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(747, 609);
+            this.Controls.Add(this.Lbl_Output);
+            this.Controls.Add(this.Lbl_Input);
+            this.Controls.Add(this.TxtB_Input);
+            this.Controls.Add(this.TxtB_Output);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MainWindow";
+            this.Text = "My PlayGround App";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -170,18 +97,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox TxtB_Output;
+        private System.Windows.Forms.TextBox TxtB_Input;
+        private System.Windows.Forms.Label Lbl_Input;
+        private System.Windows.Forms.Label Lbl_Output;
     }
 }
-
