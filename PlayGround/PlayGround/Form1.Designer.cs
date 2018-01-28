69221ab9799f5eef622904142776f91b7cd0b138
TrunksLegendary <trunks0408@gmail.com>
2018-01-28 18:58:12 -0500

    Update
    test

--- PlayGround/PlayGround/Form1.Designer.cs
+++ PlayGround/PlayGround/Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.Btn_IpSetting = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Btn_FileOpen
@@ -153,12 +154,22 @@
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(339, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // MyC_PlayGround
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(816, 614);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Btn_IpSetting);
@@ -193,5 +204,6 @@
         private System.Windows.Forms.Button Btn_IpSetting;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
